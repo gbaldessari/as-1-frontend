ba65985af1f28b98c659eb0ff66fb981b0dbd52d
Giacomo Baldessari <103346031+gbaldessari@users.noreply.github.com>
2024-08-28 20:00:27 -0400

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/Login';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <h1>Login</h1>
+            <Login />
+        </div>
+    );
+};
+
+export default App;
